f99c0b717eb5f2b383a4b86c1fbce2478030e2d0
Aayam <shrestaa@oregonstate.edu>
2020-12-23 06:37:00 -0800

    Create 365daysofconnections.m

---
+++ 365daysofconnections.m
@@ -0,0 +1,11 @@
+## Motivation: 
+As covid-19 danced around the world, 2020 has been a tough year all the way. It has been especially limiting for people looking to travel and making new connections. However, it also made us understand how connected we are due to technology, arguably more connected than ever. Hence "#365 days of connection" is my commitment to move past the circumstances and building a richer connection pool for everyone. 
+
+*Audience:* CS / ML enthusiasts. 
+*Format:* Short Video or mini-blog
+
+## Value proposition to viewers: 
+- Get Inspired Everyday. 
+- Get to know one more person.
+- Showcase the reach of AI/ML in the real world.
+- A story worth sharing.
